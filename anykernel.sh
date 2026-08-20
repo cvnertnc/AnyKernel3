52612390a7605ea81d0be6ed8846654797959ea1
cvnertnc <148134890+cvnertnc@users.noreply.github.com>
2026-08-20 12:31:38 +0300

    anykernel: some update
    - update kernel.string
    - supported.versions = 11-15
    
    - PATCH_VBMETA_FLAG = 0
    - NO_MAGISK_CHECK = 1
    - NO_VBMETA_PARTITION_PATCH = 1
    
    Signed-off-by: cvnertnc <148134890+cvnertnc@users.noreply.github.com>

--- anykernel.sh
+++ anykernel.sh
@@ -4,7 +4,7 @@
 ### AnyKernel setup
 # global properties
 properties() { '
-kernel.string=ExampleKernel by osm0sis @ xda-developers
+kernel.string=Kernels by Xiaomi Developers for Redmi Note 8 Pro (begonia).
 do.devicecheck=1
 do.modules=0
 do.systemless=1
@@ -12,7 +12,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=begonia
 device.name2=begoniain
-supported.versions=
+supported.versions=11 - 15
 supported.patchlevels=
 supported.vendorpatchlevels=
 '; } # end properties
@@ -21,7 +21,9 @@ supported.vendorpatchlevels=
 BLOCK=/dev/block/platform/bootdevice/by-name/boot;
 IS_SLOT_DEVICE=0;
 RAMDISK_COMPRESSION=auto;
-PATCH_VBMETA_FLAG=auto;
+PATCH_VBMETA_FLAG=0;
+NO_MAGISK_CHECK=1;
+NO_VBMETA_PARTITION_PATCH=1;
 
 # import functions/variables and setup patching - see for reference (DO NOT REMOVE)
 . tools/ak3-core.sh;
